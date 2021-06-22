317c4d8b4ddd452365b154ec29b467bae3fc74a9
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-06-22 17:42:18 +0530

    Update receiver_input.py

--- receiver/receiver_input.py
+++ receiver/receiver_input.py
@@ -2,7 +2,9 @@ import json
 def read_sender_input():
     try:
         input_data = input()
+        print(input_data)
         data_dict = json.loads(input_data)
+        print(f' dict: {data_dict}')
         return get_values(data_dict)             
     except EOFError:
         return None, None, None
