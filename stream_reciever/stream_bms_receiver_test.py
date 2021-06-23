347a41e85ddcde2076fdc80ba2ef161efd737433
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-06-23 09:14:11 +0530

    Update stream_bms_receiver_test.py

--- stream_reciever/stream_bms_receiver_test.py
+++ stream_reciever/stream_bms_receiver_test.py
@@ -6,17 +6,17 @@ class test_bms_stream_receiver(unittest.TestCase):
 
     def test_to_check_format(self):
 
-        self.assertTrue(read_input.check_data_type({"temperature":0.0,"soc":62.903126,"charge_rate":0.045999337}) == 'data_is_correct' )
+        self.assertTrue(read_input.check_data_type({"temperature":0.0,"soc":62.903126,"chargeRate":0.045999337}) == 'data_is_correct' )
 
     def test_to_wrong_format(self):   
         
-        self.assertTrue(read_input.check_data_type({"temperature":0.0,"soc":'abcd',"charge_rate":0.045999337}) == 'data_is_not_correct' )
-        self.assertTrue(read_input.check_data_type({"temperature":0.0,"soc":62.903126,"charge_rate":'abcd'}) == 'data_is_not_correct' )
-        self.assertTrue(read_input.check_data_type({"temperature":'abcd',"soc":62.903126,"charge_rate":0.9}) == 'data_is_not_correct' )
+        self.assertTrue(read_input.check_data_type({"temperature":0.0,"soc":'abcd',"chargeRate":0.045999337}) == 'data_is_not_correct' )
+        self.assertTrue(read_input.check_data_type({"temperature":0.0,"soc":62.903126,"chargeRate":'abcd'}) == 'data_is_not_correct' )
+        self.assertTrue(read_input.check_data_type({"temperature":'abcd',"soc":62.903126,"chargeRate":0.9}) == 'data_is_not_correct' )
 
 
 
 
 
-if __name__ == '__main__':
-  unittest.main()
+#if __name__ == '__main__':
+#  unittest.main()
